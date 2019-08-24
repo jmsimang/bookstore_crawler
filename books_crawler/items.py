55b21f6d2e00e1c0739a8b159defe9a3e6bc14f9
Jabulani Msimango <jmsimang@student.wethinkcode.co.za>
2019-08-24 20:02:38 +0200

    The generated spider item objects

---
+++ books_crawler/items.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class BooksCrawlerItem(scrapy.Item):
+    title = scrapy.Field()
+    price = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    rating = scrapy.Field()
+    description = scrapy.Field()
+    upc = scrapy.Field()
+    price_excl = scrapy.Field()
+    price_incl = scrapy.Field()
+    tax = scrapy.Field()
+    p_type = scrapy.Field()
+    stock = scrapy.Field()
+    reviews = scrapy.Field()
